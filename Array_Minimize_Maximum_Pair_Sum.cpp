6a2b1b2cc3248976db7db6624080b841d4b85d5e
pruthvi2400 <pruthvirajsonawane74@gmail.com>
2026-01-23 22:25:08 +0530

    Updated

--- Array_Minimize_Maximum_Pair_Sum.cpp
+++ Array_Minimize_Maximum_Pair_Sum.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
@@ -12,7 +17,24 @@ public:
             i++;
             j--;
         }
-
         return ans;
     }
 };
+
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution s;
+    cout << "Minimum Pair Sum = " << s.minPairSum(nums);
+
+    return 0;
+}
