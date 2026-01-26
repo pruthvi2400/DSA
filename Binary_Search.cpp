0746795e23ad56aa4f781983dd1118b57c460b49
pruthvi2400 <pruthvirajsonawane74@gmail.com>
2026-01-26 15:07:02 +0530

    New

---
+++ Binary_Search.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int Binary_Search(int arr[],int n, int key);
+
+int main()
+{
+    int n; 
+    cout<<"Enter the size of array =";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter the values in the array = ";
+        cin>>arr[i];
+    }
+
+    cout<<"Array = ";
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    cout<<endl;
+
+    int key;
+    cout<<"Enter the index of Element to Find =";
+    cin>>key;
+
+    int search = Binary_Search( arr, n, key);
+
+
+    cout<<"The index of no. is ="<<search<<endl;
+
+    return 0;
+
+}
+
+int Binary_Search(int arr[],int n,int key)
+{
+    int start=0;
+    int end=n-1;
+    int mid=(start+end)/2;   
+
+    while(start<=end)
+    {
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if(arr[mid] < key)
+        {
+            start=mid+1;
+        }
+        else{
+
+            end=mid-1;
+        }
+
+        mid=(start+end)/2;
+    }
+
+    return -1;
+}
