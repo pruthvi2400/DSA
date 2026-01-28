20f337ba67d664cb588c74ee8359d1a39e46b30a
pruthvi2400 <pruthvirajsonawane74@gmail.com>
2026-01-28 23:14:39 +0530

    New code

---
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=6;
+    int arr[]={5,6,1,2,3,4};
+
+    int start=0;
+    int end=n-1;
+    int target;
+    cout<<"Enter the no. to find = ";
+    cin >> target;
+
+    while(start <= end)
+    {
+        int mid=start+(end-start)/2;
+
+        if(arr[mid]==target)
+        {
+             cout << "Found at index: " << mid;
+             return 0;
+        }
+        if(arr[start]<=arr[mid] )
+        {
+            if(arr[start]<=target && arr[end] > target)      
+            {
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        else
+        {
+            if(arr[mid]<=target && arr[end]>target)
+            {
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    }
+
+    cout << "Element not found";
+    return 0;
+}
