167c297d927454f04b9148884299f3d6e085ccb8
pruthvi2400 <pruthvirajsonawane74@gmail.com>
2026-01-31 14:25:42 +0530

    Update

--- Sort_Selection.cpp
+++ Sort_Selection.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void Selection_sort(int arr[],int n);
+
 int main()
 {
     int n;
@@ -22,5 +24,29 @@ int main()
     
     cout<<endl;
 
+    Selection_sort( arr, n);
+    cout<<"The sorted Array =";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+
 }
 
+void Selection_sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int minindex=i;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[minindex]>arr[j])
+            minindex=j;
+        }
+        swap(arr[minindex],arr[i]);
+    }
+}
