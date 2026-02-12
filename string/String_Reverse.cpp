131feecb0059748ee36a88c7e999d650e1111900
pruthvi2400 <pruthvirajsonawane74@gmail.com>
2026-02-12 14:29:39 +0530

    String Reversing

---
+++ string/String_Reverse.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+//For reversing a string you need to find its lenght first
+
+int str_Lenght(char name[])
+{
+    int count =0;
+    for(int i=0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    return count;
+}
+
+int str_Reverse(char name[],int n)
+{
+    int start=0;
+    int end=n-1;
+
+    while(start<end)
+    {
+        swap(name[start],name[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    char name[10];
+
+    cout<<"Enter the name :";
+    cin>>name;
+
+    cout<<"IS your name :";
+    cout<<name<<endl;
+
+    int len= str_Lenght( name );
+
+    cout<<"Length of String :"<<len<<endl;
+
+    str_Reverse(name,len);
+    cout<<"The reverse String :";
+    cout<<name<<endl;
+
+    return 0;
+
+
+
+}
